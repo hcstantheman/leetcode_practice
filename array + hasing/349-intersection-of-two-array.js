b99c25e438c0f70138e25a91befb3c46360ba912
gstanchau <hcstanleychau@gmail.com>
2025-09-14 18:42:19 +0800

    addition of the comment

--- array + hasing/349-intersection-of-two-array.js
+++ array + hasing/349-intersection-of-two-array.js
@@ -9,3 +9,7 @@ var intersection = function(nums1, nums2) {
 
 // set comes with some intersection, union, functions from the set theory
 // hope you all are familiar with the Venn Diagram..
+
+// if that is a bit too hard to see, you are invited to do more variable declaration
+// let set1 = new Set(nums1), etc..
+// and finally just return the value in the arr form
